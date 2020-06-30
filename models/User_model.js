e2d18a14d6af5b16e7c4966c8d22614964657394
Damaris Goebel <damaris.goebel@gmx.de>
2020-06-30 19:15:08 +0200

    start-auth

--- models/User_model.js
+++ models/User_model.js
@@ -1,16 +1,29 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
   username: String,
   email: String,
   password: String,
-  profilepic: String
+  profilePic: String,
+
+  googleID: String, 
+  verifiedEmail: { type: Boolean, default: false },
+  token: String
+  },
+
+  {
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+
   //,
   //activityfeed: [{ type: Schema.Types.ObjectId, ref: 'Activity' }],
   //owner: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
-const User = mongoose.model('User', userSchema);
 
+const User = mongoose.model('User', userSchema);
 module.exports = User;
